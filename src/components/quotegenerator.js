5ad86ecd8f840897199f037409fb86cf8043fb80
km00776 <kmurugathas@gmail.com>
2020-10-20 22:58:57 +0100

    Starting quotegenerator

---
+++ src/components/quotegenerator.js
@@ -0,0 +1,15 @@
+import React, {Fragment} from 'react';
+import 'semantic-ui-css/semantic.min.css'
+
+export default function QuoteGenerator() {
+    return (
+      <Fragment>
+        <div className="container">
+          <h1>Hello</h1>
+        
+        </div>
+      </Fragment>
+    );
+  }
+  
+ 
--- src/index.js
+++ src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import QuoteGenerator from './components/quotegenerator';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import 'semantic-ui-css/semantic.min.css'
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+
+
+ReactDOM.render(<QuoteGenerator/>, document.getElementById('root'));
