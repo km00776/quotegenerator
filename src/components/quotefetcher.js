dd6b7baf32aeddf701f095d4404ca28b7389fb41
km00776 <kmurugathas@gmail.com>
2020-10-22 23:39:10 -0700

    made massive changes on variables

--- src/components/quotefetcher.js
+++ src/components/quotefetcher.js
@@ -1,18 +1,26 @@
-import React, {Fragment} from 'react';
-import 'semantic-ui-css/semantic.min.css';
-import QuoteGenerator from './quotegenerator';
+import React, { Fragment } from "react";
+import "semantic-ui-css/semantic.min.css";
+import QuoteGenerator from "./quotegenerator";
 
 export default class QuoteFetcher extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            quote: "",
-            quoteAuthor:""
-        }
-    }
-    render() {
-        return(
-            <QuoteGenerator quote={this.state.quote} quoteAuthor={this.state.quoteAuthor}></QuoteGenerator>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: "test",
+      quoteAuthor: "",
+    };
+  }
+
+  componentDidMount() {
+    fetch("").then((res) => res.json);
+  }
+
+  render() {
+    return (
+      <QuoteGenerator
+        quote={this.state.quote}
+        quoteAuthor={this.state.quoteAuthor}
+      ></QuoteGenerator>
+    );
+  }
 }
--- src/components/quotegenerator.js
+++ src/components/quotegenerator.js
@@ -1,17 +1,14 @@
-import React, {Fragment} from 'react';
-import 'semantic-ui-css/semantic.min.css'
+import React, { Fragment } from "react";
+import "semantic-ui-css/semantic.min.css";
 
 export default function QuoteGenerator(props) {
-    return (
-      <Fragment>
-        <div id="quote-box">
-            <p id ="text">{this.props.quote}</p>
-          <p id ="text">   </p>
-          <button id="quote-box"></button>
-        </div>
-        
-      </Fragment>
-    );
-  }
-  
- 
+  return (
+    <Fragment>
+      <div id="quote-box">
+        <p id="text">{props.quote}</p>
+        <p id="text"> </p>
+        <button id="quote-box"></button>
+      </div>
+    </Fragment>
+  );
+}
